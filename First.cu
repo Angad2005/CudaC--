105409c21f8b74b0635de128633a38aa689faa8c
angadsingh7666 <angdsingh7666@gmail.com>
2026-07-16 23:11:03 +0530

    Hello Friend

--- First.cu
+++ First.cu
@@ -1,5 +1,21 @@
 #include<iostream>
+
+// Cuda library
 #include<cuda.h>
-__global__ void kernel(int *a, int *b, int *c) {
+using namespace std;
+// Cuda kernel function which runs on the GPU
+__global__ void myFirstKernel() {
+    printf("Hello World from GPU!\n");
     
+}
+
+int main() {
+    cout<<"Hello World from CPU!"<<endl;
+    // Launch the kernel with 1 block and 1 thread
+    myFirstKernel<<<1, 1>>>();
+    
+    // Wait for the GPU to finish before accessing on host
+    cudaDeviceSynchronize();
+    
+    return 0;
 }
