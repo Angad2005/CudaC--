79349f1ad158189b0723b656d82a0384a199d761
angadsingh7666 <angdsingh7666@gmail.com>
2026-07-18 09:12:59 +0530

    Vector Addition

---
+++ vectorAddition.cu
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cuda.h>
+using namespace std;
+
+
+//const word used to say rea donly from the input vector and pointers pont to the arrays in the GPU memory
+__global__ void kernel(const float*a, const float*b, float*c, int n){
+
+}
+
+
+
+int main(){
+    return 0;
+}
